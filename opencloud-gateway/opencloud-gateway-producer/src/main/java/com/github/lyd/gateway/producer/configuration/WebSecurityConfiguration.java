77588718a4fc3df7984fbbdd16dcc7ea37687831
liuyadu <515608851@qq.com>
2018-12-13 19:02:14 +0800

    覆盖样式

--- opencloud-gateway/opencloud-gateway-producer/src/main/java/com/github/lyd/gateway/producer/configuration/WebSecurityConfiguration.java
+++ opencloud-gateway/opencloud-gateway-producer/src/main/java/com/github/lyd/gateway/producer/configuration/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.lyd.gateway.producer.configuration;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @date: 2018/10/23 10:31
  * @description:
  */
+@EnableOAuth2Sso
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
