155349fdd5860986c501dda36760e304860f7992
liuyadu <515608851@qq.com>
2018-12-19 13:49:12 +0800

    修改异常

--- opencloud-upms/opencloud-rbac-producer/src/main/java/com/github/lyd/rbac/producer/RbacApplication.java
+++ opencloud-upms/opencloud-rbac-producer/src/main/java/com/github/lyd/rbac/producer/RbacApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
@@ -13,6 +14,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.github.lyd"})
 @MapperScan(basePackages = "com.github.lyd.rbac.producer.mapper")
 public class RbacApplication {
 
