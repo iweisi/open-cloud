72b879bbc9c167ece0af510cdd506d5a3df59f79
liu.yadu <515608851@qq.com>
2018-12-20 21:41:00 +0800

    重构

--- docs/bin/boot-strap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#description:开机自启脚本
-#启动tomcat
-/root/deploy/service/springboot.sh start opencloud-discovery.jar
-#启动tomcat
-/root/deploy/service/springboot.sh start opencloud-config.jar
