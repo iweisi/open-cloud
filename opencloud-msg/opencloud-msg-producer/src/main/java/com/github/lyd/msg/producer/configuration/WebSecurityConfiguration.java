71833467c704daff283bc0f9f15ec8ad87884374
liu.yadu <515608851@qq.com>
2018-12-28 00:05:33 +0800

    开启Mybatis的二级缓存

---
+++ opencloud-msg/opencloud-msg-producer/src/main/java/com/github/lyd/msg/producer/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.github.lyd.msg.producer.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author: liuyadu
+ * @date: 2018/10/23 10:31
+ * @description:
+ */
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/", "/**/v2/api-docs/**", "/swagger-resources/**",
+                "/swagger-ui.html", "/webjars/**", "/**/favicon.ico");
+    }
+
+}
