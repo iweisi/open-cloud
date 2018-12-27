79c15279f4520264f2f50a5e6c1169d269a566ad
liu.yadu <515608851@qq.com>
2018-12-27 23:07:40 +0800

    开启Mybatis的二级缓存

--- opencloud-gateway/opencloud-gateway-producer/src/main/java/com/github/lyd/gateway/producer/mapper/GatewayRateLimitMapper.java
+++ opencloud-gateway/opencloud-gateway-producer/src/main/java/com/github/lyd/gateway/producer/mapper/GatewayRateLimitMapper.java
@@ -2,11 +2,13 @@ package com.github.lyd.gateway.producer.mapper;
 
 import com.github.lyd.common.mapper.CrudMapper;
 import com.github.lyd.gateway.client.entity.GatewayRateLimit;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author liuyadu
  */
 @Repository
+@CacheNamespace
 public interface GatewayRateLimitMapper extends CrudMapper<GatewayRateLimit> {
 }
--- opencloud-gateway/opencloud-gateway-producer/src/main/java/com/github/lyd/gateway/producer/mapper/GatewayRoutesMapper.java
+++ opencloud-gateway/opencloud-gateway-producer/src/main/java/com/github/lyd/gateway/producer/mapper/GatewayRoutesMapper.java
@@ -2,11 +2,13 @@ package com.github.lyd.gateway.producer.mapper;
 
 import com.github.lyd.common.mapper.CrudMapper;
 import com.github.lyd.gateway.client.entity.GatewayRoute;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author liuyadu
  */
 @Repository
+@CacheNamespace
 public interface GatewayRoutesMapper extends CrudMapper<GatewayRoute> {
 }
