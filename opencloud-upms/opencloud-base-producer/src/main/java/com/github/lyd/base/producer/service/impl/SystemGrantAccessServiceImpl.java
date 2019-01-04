0631e689e40d848cae5ed8561afd9baf66bafee8
liu.yadu <515608851@qq.com>
2019-01-04 21:01:40 +0800

    修改APP授权,解决授权批量添加 主键为空

--- opencloud-upms/opencloud-base-producer/src/main/java/com/github/lyd/base/producer/service/impl/SystemGrantAccessServiceImpl.java
+++ opencloud-upms/opencloud-base-producer/src/main/java/com/github/lyd/base/producer/service/impl/SystemGrantAccessServiceImpl.java
@@ -350,27 +350,8 @@ public class SystemGrantAccessServiceImpl implements SystemGrantAccessService {
             code = api.getApiCode();
         }
         if (object != null) {
-            if (authorityPrefix != null) {
-                if (BaseConstants.AUTHORITY_PREFIX_ROLE.equals(authorityPrefix)) {
-                    SystemRole role = systemRoleService.getRole(Long.parseLong(authorityOwner));
-                    // 角色授权标识=ROLE_角色编码
-                    authority = authorityPrefix + role.getRoleCode();
-                } else if (BaseConstants.AUTHORITY_PREFIX_USER.equals(authorityPrefix)) {
-                    // 个人授权权限标识=USER_用户ID_资源类型_资源编码 => USER_1_MENU_LIST
-                    authority = authorityPrefix + authorityOwner + BaseConstants.AUTHORITY_SEPARATOR + resourceType + BaseConstants.AUTHORITY_SEPARATOR + resourceId;
-                } else {
-                    // APP授权状态
-                    if (BaseConstants.AUTHORITY_PREFIX_APP.equals(authorityPrefix)) {
-                        SystemApp systemApp = systemAppMapper.selectByPrimaryKey(authorityOwner);
-                        if (systemApp != null) {
-                            // 根据APP的状态强制覆盖
-                            status = systemApp.getStatus();
-                        }
-                    }
-                    // 默认权限标识:前缀+资源编码
-                    authority = authorityPrefix + code;
-                }
-            }
+            // 默认权限标识:前缀+资源编码
+            authority = authorityPrefix + code;
             grantAccess = new SystemGrantAccess();
             grantAccess.setServiceId(serviceId);
             grantAccess.setResourceId(resourceId);
