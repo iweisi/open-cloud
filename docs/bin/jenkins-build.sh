f787f1f0b9fd26a8f1c96ff529481d0a84338233
liuyadu <515608851@qq.com>
2019-01-22 17:20:53 +0800

    更新sql

--- docs/bin/jenkins-build.sh
+++ docs/bin/jenkins-build.sh
@@ -1,3 +1,5 @@
-cp -rf opencloud-upms/$JOB_NAME/target/$JOB_NAME.jar /root/deploy
+cd /root/deploy
+./startup.sh stop $JOB_NAME.jar
+cp -rf $WORKSPACE/opencloud-gateway/$JOB_NAME/target/$JOB_NAME.jar /root/deploy
 cd /root/deploy
 ./startup.sh restart $JOB_NAME.jar
